c69f7057f2bada935e83e44aab963a37d3869323
Tom Whitwell <tom@whi.tw>
2020-07-21 18:47:27 +0100

    working better, but delays aren't

--- rgbled.py
+++ rgbled.py
@@ -1,7 +1,6 @@
-import random
 import time
+
 import machine
-import math
 import uasyncio as asyncio
 
 
@@ -15,78 +14,45 @@ class rgbled:
 
     def setup(self, rpin, gpin, bpin, freq):
         self.RED = machine.PWM(machine.Pin(self.rpin), freq=self.freq)
+        self.RED.deinit()
         self.GREEN = machine.PWM(machine.Pin(self.gpin), freq=self.freq)
+        self.GREEN.deinit()
         self.BLUE = machine.PWM(machine.Pin(self.bpin), freq=self.freq)
+        self.BLUE.deinit()
         self.frequency = freq
-        self.redprev = 1
-        self.greenprev = 1
-        self.blueprev = 1
 
         self.PWMMAX = 1023
 
     async def killer(self, time):
         await asyncio.sleep(time)
 
-    def changeto(self, redv, greenv, bluev, speed):
-        r = self.rgb_to_duty(redv)
-        g = self.rgb_to_duty(greenv)
-        b = self.rgb_to_duty(bluev)
-        if(r == self.redprev or r == 0):
-            rx = self.redprev + 1
-        else:
-            rx = abs(r-self.redprev)
-        if(g == self.greenprev or g == 0):
-            gx = self.greenprev + 1
-        else:
-            gx = abs(g-self.greenprev)
-        if(b == self.blueprev or b == 0):
-            bx = self.blueprev + 1
-        else:
-            bx = abs(b-self.blueprev)
-        rs = speed/rx
-        gs = speed/gx
-        bs = speed/bx
+    def changeto(self, r, g, b, t):
         loop = asyncio.get_event_loop()
-        loop.create_task(self.changered(r, rs))
-        loop.create_task(self.changegreen(g, gs))
-        loop.create_task(self.changeblue(b, bs))
-        loop.run_until_complete(self.killer(speed))
+        loop.create_task(self.transition(r, t, self.RED))
+        loop.create_task(self.transition(g, t, self.GREEN))
+        loop.create_task(self.transition(b, t, self.BLUE))
+        loop.run_until_complete(self.killer(t))
 
-    async def changered(self, red, speed):
-        if(red > self.redprev):
-            for x in range(self.redprev, red):
-                self.RED.duty(x)
-                time.sleep(speed)
+    async def transition(self, value: int, t: int, led: machine.PWM):
+        current_duty = led.duty()
+        target_duty = self.rgb_to_duty(value)
+        try:
+            steps = abs(current_duty - target_duty)
+            step_time = t / steps
+        except ZeroDivisionError:
+            steps = 0
+            step_time = 0
+        print(current_duty, target_duty, steps, step_time)
+        if(target_duty > current_duty):
+            for x in range(current_duty, target_duty+1):
+                print(x)
+                led.duty(x)
+                asyncio.sleep(step_time)
         else:
-            down = self.redprev - red
+            down = current_duty - target_duty
             for x in range(0, down):
-                self.RED.duty(self.redprev - x)
-                time.sleep(speed)
-        self.redprev = red
-
-    async def changegreen(self, green, speed):
-        if(green > self.greenprev):
-            for x in range(self.greenprev, green):
-                self.GREEN.duty(x)
-                time.sleep(speed)
-        else:
-            down = self.greenprev - green
-            for x in range(0, down):
-                self.GREEN.duty(self.greenprev - x)
-                time.sleep(speed)
-        self.greenprev = green
-
-    async def changeblue(self, blue, speed):
-        if(blue > self.blueprev):
-            for x in range(self.blueprev, blue):
-                self.BLUE.duty(x)
-                time.sleep(speed)
-        else:
-            down = self.blueprev - blue
-            for x in range(0, down):
-                self.BLUE.duty(self.blueprev - x)
-                time.sleep(speed)
-        self.blueprev = blue
+                led.duty(current_duty - x)
+                asyncio.sleep(step_time)
 
     def on(self, r, g, b, speed):
         self.setup(self.rpin, self.gpin, self.bpin, self.freq)
@@ -99,5 +65,5 @@ class rgbled:
 
     def rgb_to_duty(self, val):
         new = int(abs((val * self.PWMMAX/255) - 1023))
-        print(val, new)
+        # print(val, new)
         return new
