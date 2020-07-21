77ac7ab6395447d2b3581b597863e552b392aec0
Tom Whitwell <tom@whi.tw>
2020-07-21 22:12:25 +0100

    works! just times aren't great

--- rgbled.py
+++ rgbled.py
@@ -23,36 +23,44 @@ class rgbled:
 
         self.PWMMAX = 1023
 
-    async def killer(self, time):
-        await asyncio.sleep(time)
+    async def killer(self, r: (machine.PWM, int), g: (machine.PWM, int), b: (machine.PWM, int)):
+        print(r, g, b)
+        while True:
+            if r[0].duty() == r[1] and g[0].duty() == g[1] and b[0].duty() == b[1]:
+                print("bums")
+                return
+            await asyncio.sleep(0.1)
 
     def changeto(self, r, g, b, t):
         loop = asyncio.get_event_loop()
         loop.create_task(self.transition(r, t, self.RED))
         loop.create_task(self.transition(g, t, self.GREEN))
         loop.create_task(self.transition(b, t, self.BLUE))
-        loop.run_until_complete(self.killer(t))
+        loop.run_until_complete(self.killer(
+            (self.RED, self.rgb_to_duty(r)),
+            (self.GREEN, self.rgb_to_duty(g)),
+            (self.BLUE, self.rgb_to_duty(b))
+        ))
+        loop.close()
 
     async def transition(self, value: int, t: int, led: machine.PWM):
+        # TODO: step through RGB values, not duty
         current_duty = led.duty()
         target_duty = self.rgb_to_duty(value)
-        try:
-            steps = abs(current_duty - target_duty)
-            step_time = t / steps
-        except ZeroDivisionError:
-            steps = 0
-            step_time = 0
+        steps = abs(target_duty - current_duty)
+        if steps == 0:
+            return
+        step_time = t / steps
         print(current_duty, target_duty, steps, step_time)
         if(target_duty > current_duty):
             for x in range(current_duty, target_duty+1):
-                print(x)
                 led.duty(x)
-                asyncio.sleep(step_time)
+                await asyncio.sleep(step_time)
         else:
             down = current_duty - target_duty
-            for x in range(0, down):
+            for x in range(0, down+1):
                 led.duty(current_duty - x)
-                asyncio.sleep(step_time)
+                await asyncio.sleep(step_time)
 
     def on(self, r, g, b, speed):
         self.setup(self.rpin, self.gpin, self.bpin, self.freq)
@@ -67,3 +75,7 @@ class rgbled:
         new = int(abs((val * self.PWMMAX/255) - 1023))
         # print(val, new)
         return new
+
+
+# TODO: Define some kind of duty_to_rgb(self, duty) \
+# TODO: This will need to convert a given duty to the closest RGB value
