0ef726714c8235414e32b6562c908078e7682589
Tom Whitwell <tom@whi.tw>
2020-07-22 12:06:14 +0100

    working with webui

---
+++ main.py
@@ -0,0 +1,100 @@
+# Complete project details at https://RandomNerdTutorials.com
+import re
+import gc
+
+import wifimgr
+import machine
+
+from rgbled import rgbled
+
+try:
+    import usocket as socket
+except:
+    import socket
+
+led = machine.Pin(2, machine.Pin.OUT)
+
+wlan = wifimgr.get_connection()
+if wlan is None:
+    print("Could not initialize the network connection.")
+    while True:
+        pass  # you shall not pass :D
+
+# Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
+print("ESP OK")
+
+
+led = rgbled(0, 5, 2)
+
+
+def web_page():
+    red = led.RED.state()
+    green = led.GREEN.state()
+    blue = led.BLUE.state()
+
+    html = """<html>
+  <head>
+    <title>ESP Web Server</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="data:," />
+    <script src="https://cdn.jsdelivr.net/npm/@jaames/iro@5"></script>
+  </head>
+  <body>
+    <h1>ESP Web Server</h1>
+    <div id="picker"></div>
+    <script>
+      var colorPicker = new iro.ColorPicker("#picker", {{
+        color: "rgb({red},{green},{blue})",
+      }});
+      colorPicker.on("input:end", function(color) {{
+        color = color.rgb;
+        const Http = new XMLHttpRequest();
+        const url = "/?r=" + color.r + "&g=" + color.g + "&b=" + color.b;
+        console.log(url);
+        Http.open("GET", url);
+        Http.send();
+      }});
+    </script>
+  </body>
+</html>
+"""
+    return html.format(red=red, green=green, blue=blue)
+
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('', 80))
+    s.listen(5)
+except OSError as e:
+    machine.reset()
+
+r = re.compile(r'GET\s\/\?r=(\d+)\&g\=(\d+)\&b\=(\d+)')
+while True:
+    try:
+        if gc.mem_free() < 102000:
+            gc.collect()
+        conn, addr = s.accept()
+        conn.settimeout(3.0)
+        print('Got a connection from %s' % str(addr))
+        request = conn.recv(1024)
+        conn.settimeout(None)
+        print(request)
+        m = r.match(request)
+        if m:
+            print("interesting request")
+            red = int(m.group(1))
+            green = int(m.group(2))
+            blue = int(m.group(3))
+            led.changeto(red, green, blue)
+            response = "OK"
+        else:
+            response = web_page()
+        conn.send('HTTP/1.1 200 OK\n')
+        conn.send('Content-Type: text/html\n')
+        conn.send('Connection: close\n\n')
+        conn.sendall(response)
+        conn.close()
+    except OSError as e:
+        conn.close()
+        print('Connection closed')
--- rgbled.py
+++ rgbled.py
@@ -1,81 +1,95 @@
-import time
-
 import machine
 import uasyncio as asyncio
 
 
+class CHANNEL:
+    def __init__(self, color: int, pin: int, frequency: int, pwmmax: int, duty_map: {int: int}):
+        self.FREQUENCY = frequency
+        self.COLOR = color
+        self.PIN = pin
+        self.PWMMAX = pwmmax
+        self._state = 0
+        self.DUTY_MAP = duty_map
+        self.setup()
+
+    def setup(self):
+        self.PWM = machine.PWM(machine.Pin(self.PIN),
+                               freq=self.FREQUENCY, duty=self.PWMMAX)
+
+    def state(self, new_state=None):
+        if new_state is not None:
+            self.PWM.duty(self.DUTY_MAP[new_state])
+            self._state = new_state
+        return self._state
+
+
 class rgbled:
+    RED = 0
+    GREEN = 1
+    BLUE = 2
+
     def __init__(self, rpin, gpin, bpin):
         self.rpin = rpin
         self.gpin = gpin
         self.bpin = bpin
-        self.freq = 1000
-        self.setup(self.rpin, self.gpin, self.bpin, self.freq)
-
-    def setup(self, rpin, gpin, bpin, freq):
-        self.RED = machine.PWM(machine.Pin(self.rpin), freq=self.freq)
-        self.RED.deinit()
-        self.GREEN = machine.PWM(machine.Pin(self.gpin), freq=self.freq)
-        self.GREEN.deinit()
-        self.BLUE = machine.PWM(machine.Pin(self.bpin), freq=self.freq)
-        self.BLUE.deinit()
-        self.frequency = freq
-
+        self.FREQUENCY = 1000
         self.PWMMAX = 1023
+        self.setup(self.rpin, self.gpin, self.bpin, self.FREQUENCY)
 
-    async def killer(self, r: (machine.PWM, int), g: (machine.PWM, int), b: (machine.PWM, int)):
-        print(r, g, b)
+    def setup(self, rpin, gpin, bpin, frequency):
+        duty_map = {}
+        for i in range(0, 256):
+            duty_map[i] = self.rgb_to_duty(i)
+
+        self.RED = CHANNEL(rgbled.RED, rpin, frequency,
+                           self.PWMMAX, duty_map)
+        self.GREEN = CHANNEL(rgbled.GREEN, gpin, frequency,
+                             self.PWMMAX, duty_map)
+        self.BLUE = CHANNEL(rgbled.BLUE, bpin, frequency,
+                            self.PWMMAX, duty_map)
+
+    async def killer(self, r: (CHANNEL, int), g: (CHANNEL, int), b: (CHANNEL, int)):
         while True:
-            if r[0].duty() == r[1] and g[0].duty() == g[1] and b[0].duty() == b[1]:
-                print("bums")
+            if r[0].state() == r[1] and g[0].state() == g[1] and b[0].state() == b[1]:
                 return
             await asyncio.sleep(0.1)
 
-    def changeto(self, r, g, b, t):
+    def changeto(self, r: int, g: int, b: int, time=0):
+        await self._changeto(r, g, b, time)
+
+    async def _changeto(self, r: int, g: int, b: int, time=0):
         loop = asyncio.get_event_loop()
-        loop.create_task(self.transition(r, t, self.RED))
-        loop.create_task(self.transition(g, t, self.GREEN))
-        loop.create_task(self.transition(b, t, self.BLUE))
+        loop.create_task(self.transition(r, time, self.RED))
+        loop.create_task(self.transition(g, time, self.GREEN))
+        loop.create_task(self.transition(b, time, self.BLUE))
         loop.run_until_complete(self.killer(
-            (self.RED, self.rgb_to_duty(r)),
-            (self.GREEN, self.rgb_to_duty(g)),
-            (self.BLUE, self.rgb_to_duty(b))
+            (self.RED, r),
+            (self.GREEN, g),
+            (self.BLUE, b)
         ))
         loop.close()
 
-    async def transition(self, value: int, t: int, led: machine.PWM):
-        # TODO: step through RGB values, not duty
-        current_duty = led.duty()
-        target_duty = self.rgb_to_duty(value)
-        steps = abs(target_duty - current_duty)
+    async def transition(self, value: int, time: int, led: CHANNEL):
+        old = led.state()
+        target = value
+        steps = abs(target - old)
         if steps == 0:
             return
-        step_time = t / steps
-        print(current_duty, target_duty, steps, step_time)
-        if(target_duty > current_duty):
-            for x in range(current_duty, target_duty+1):
-                led.duty(x)
+        step_time = time / steps
+        if time == 0:
+            led.state(value)
+            return
+        if(target > old):
+            # Going up!
+            for x in range(old, target+1):
+                led.state(x)
                 await asyncio.sleep(step_time)
         else:
-            down = current_duty - target_duty
-            for x in range(0, down+1):
-                led.duty(current_duty - x)
+            # going down
+            for x in range(old, target-1, -1):
+                led.state(x)
                 await asyncio.sleep(step_time)
-
-    def on(self, r, g, b, speed):
-        self.setup(self.rpin, self.gpin, self.bpin, self.freq)
-        time.sleep(0.001)
-        self.changeto(r, g, b, speed)
-
-    def off(self, speed):
-        self.changeto(1, 1, 1, speed)
-        time.sleep(speed)
 
     def rgb_to_duty(self, val):
         new = int(abs((val * self.PWMMAX/255) - 1023))
-        # print(val, new)
         return new
-
-
-# TODO: Define some kind of duty_to_rgb(self, duty) \
-# TODO: This will need to convert a given duty to the closest RGB value
